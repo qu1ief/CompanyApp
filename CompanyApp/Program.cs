ee364150fed7e50229de785d2a8e04afcb8e1a05
Sabir164 <sabirkg@code.edu.az>
2024-01-05 22:45:16 -0800

    project hazirdir

--- CompanyApp/Program.cs
+++ CompanyApp/Program.cs
@@ -3,6 +3,7 @@ using CompanyApp.Domain.Entities;
 using CompanyAppHelper;
 
 DepartmentService _departmentService = new();
+EmployeeService _employeeService = new();
 
 Department department = new()
 {
@@ -72,9 +73,63 @@ switch (menu)
         _printDepartments(_departmentService.GetAll());
         goto restart;
 
+    case "8":
+        Console.Clear();
+        var createEmployee = _createEmployee(_departmentService);
+        if (createEmployee is not null)
+        {
+            _employeeService.Create(createEmployee);
+        }
+        goto restart;
+
+    case "9":
+        Console.Clear();
+        var updatedEmployee = _updateEmployee(_departmentService, _employeeService);
+        if (updatedEmployee is not null)
+        {
+            _employeeService.Update(updatedEmployee);
+        }
+        goto restart;
+
+    case "10":
+        Console.Clear();
+        var deletedEmployee = _deleteEmployee(_employeeService);
+        if (deletedEmployee is not null)
+        {
+            _employeeService.Delete(deletedEmployee.Id);
+        }
+        goto restart;
+
+
+    case "11":
+        Console.Clear();
+        int id = _getById();
+        var employee = _employeeService.Get(id);
+        if (employee is not null)
+        {
+            Helper.ChangeTextColor(ConsoleColor.White, $"{employee.Id}. {employee.Name} {employee.Surname} {employee.Age} email:{employee.Address} -Department:{employee.Department.Name} ");
+        }
+        goto restart;
+
+    case "12":
+        Console.Clear();
+        var adressEmployee = _employeeService.GetByAddress(_search());
+        if (adressEmployee is not null)
+        {
+            Helper.ChangeTextColor(ConsoleColor.Yellow, adressEmployee.ToString());
+        }
+        goto restart;
+    case "13":
+        Console.Clear();
+        _printEmployees(_employeeService.GetAll(_search()));
+        goto restart; ;
+
+    case "14":
+        _printEmployees(_employeeService.GetAll());
+        goto restart;
 
     case "15":
-        Helper.ChangeTextColor(ConsoleColor.Magenta, "Goodbye" );
+        Helper.ChangeTextColor(ConsoleColor.Magenta, "Goodbye");
         break;
     default:
         Helper.ChangeTextColor(ConsoleColor.DarkRed, "Please enter valid number");
@@ -85,6 +140,87 @@ switch (menu)
 
 
 
+
+static Employee _createEmployee(DepartmentService departmentService)
+{
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Employee adi daxil edin");
+    string name = Console.ReadLine();
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Employee soyadi daxil edin");
+    string surname = Console.ReadLine();
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Employee yashi daxil edin");
+    byte age;
+    bool isParse = byte.TryParse(Console.ReadLine(), out age);
+    if (!isParse)
+    {
+        Helper.ChangeTextColor(ConsoleColor.Red, "Duzgun eded daxil edin");
+        return null;
+    }
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Employee addressi daxil edin");
+    string address = Console.ReadLine();
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Department secin");
+_selectDepartment:
+    var department = _selectDepartment(departmentService);
+    if (department == null)
+    {
+        Helper.ChangeTextColor(ConsoleColor.Red, "duzgun secim edin");
+        goto _selectDepartment;
+    }
+
+    Employee employee = new()
+    {
+        Name = name,
+        Surname = surname,
+        Address = address,
+        Age = age,
+        Department = department,
+        DepartmentId = department.Id,
+    };
+    return employee;
+}
+static Employee _updateEmployee(DepartmentService departmentService, EmployeeService employeeService)
+{
+    var employee = _selectEmployee(employeeService);
+    if (employee is null)
+    {
+        return null;
+    }
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Employee adi daxil edin");
+    string name = Console.ReadLine();
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Employee soyadi daxil edin");
+    string surname = Console.ReadLine();
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Employee yashi daxil edin");
+    byte age;
+    bool isParse = byte.TryParse(Console.ReadLine(), out age);
+    if (!isParse)
+    {
+        Helper.ChangeTextColor(ConsoleColor.Red, "Duzgun eded daxil edin");
+        return null;
+    }
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Employee addressi daxil edin");
+    string address = Console.ReadLine();
+    Helper.ChangeTextColor(ConsoleColor.Yellow, "Department secin");
+_selectDepartment:
+    var department = _selectDepartment(departmentService);
+    if (department == null)
+    {
+        Helper.ChangeTextColor(ConsoleColor.Red, "duzgun secim edin");
+        goto _selectDepartment;
+    }
+
+    employee.Name = name;
+    employee.Surname = surname;
+    employee.Age = age;
+    employee.Address = address;
+    employee.Department = department;
+    employee.DepartmentId = department.Id;
+    return employee;
+}
+static Employee _deleteEmployee(EmployeeService employeeService)
+{
+    Helper.ChangeTextColor(ConsoleColor.Red, "Sileceyiniz Employeeni secin");
+    var employee = _selectEmployee(employeeService);
+    return employee;
+}
 static Department _createDepartment()
 {
     Helper.ChangeTextColor(ConsoleColor.Yellow, "Department adi daxil edin");
@@ -198,6 +334,29 @@ static Department _selectDepartment(DepartmentService departmentService)
 
 }
 
+
+static Employee _selectEmployee(EmployeeService employeeService)
+{
+    var employees = employeeService.GetAll();
+    for (int i = 0; i < employees.Count; i++)
+    {
+        Helper.ChangeTextColor(ConsoleColor.White, $"{employees[i].Id}.{employees[i]}");
+
+    }
+    int employeeId;
+    bool tryParse = int.TryParse(Console.ReadLine(), out employeeId);
+    if (!tryParse)
+    {
+        Helper.ChangeTextColor(ConsoleColor.Red, "Duzgun eded daxil edin");
+        return null;
+
+    }
+    var existed = employeeService.Get(employeeId);
+
+    return existed;
+
+}
+
 static void _printMenu()
 {
 
@@ -209,7 +368,13 @@ static void _printMenu()
     Helper.ChangeTextColor(ConsoleColor.Yellow, "6- GetEmployeeByDepartment");
     Helper.ChangeTextColor(ConsoleColor.Yellow, "7- GetAllDepartments \n");
 
- 
+    Helper.ChangeTextColor(ConsoleColor.Cyan, "8- CreateEmployee");
+    Helper.ChangeTextColor(ConsoleColor.Cyan, "9- UpdateEmployee");
+    Helper.ChangeTextColor(ConsoleColor.Cyan, "10- DeleteEmployee");
+    Helper.ChangeTextColor(ConsoleColor.Cyan, "11- GetEmployeeById");
+    Helper.ChangeTextColor(ConsoleColor.Cyan, "12- GetEmployeeByAdress");
+    Helper.ChangeTextColor(ConsoleColor.Cyan, "13- SearchEmployee");
+    Helper.ChangeTextColor(ConsoleColor.Cyan, "14- GetAllEmployee");
 
     Helper.ChangeTextColor(ConsoleColor.Red, "\n15- ExitProgram \n");
 
